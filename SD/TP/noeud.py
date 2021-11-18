56dc59e035b5e0bf2e50e2f1b338965958456bd7
paulparentdc <paul.parentdc@gmail.com>
2021-11-18 13:38:16 +0100

    aj

---
+++ SD/TP/noeud.py
@@ -0,0 +1,16 @@
+def class noeud:
+    _add = None
+
+    def __init__(self, ipNoeudLiaison, portNoeudLiaison)
+
+    def put(self, key, val, add):
+        pass
+    
+    def join(self, key, add):
+        pass
+
+    def quit(self, key, msgGet, msgPut, mg):
+        pass
+
+    def new(self, key, add):
+        pass
