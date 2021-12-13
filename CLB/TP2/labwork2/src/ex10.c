9270fdf86df10a7410b40f5662e170a3c0a9553c
paulparentdc <paul.parentdc@gmail.com>
2021-12-13 15:29:36 +0100

    fin TP2 CLB

--- CLB/TP2/labwork2/src/ex10.c
+++ CLB/TP2/labwork2/src/ex10.c
@@ -18,6 +18,26 @@
 // GPIODA
 #define USER_BUT	0
 
+//Set gpiod mode to given mode
+void set_gpiodMode(int gpio, uint8_t mode){
+	GPIOD_MODER = SET_BITS(GPIOD_MODER, gpio*2, 2, mode);
+}
+
+//Set gpiod to pushpull type
+void set_gpiodType_pushpull(int gpio){
+	GPIOD_OTYPER &= ~(1<<gpio);
+}
+
+//Turn on gpiod
+void turnd_on(int gpio){
+	GPIOD_BSRR = 1<<gpio;
+}
+
+//Turn off gpiod
+void turnd_off(int gpio){
+	GPIOD_BSRR = 1<<(16+gpio);
+}
+
 
 int main() {
 	printf("\nStarting...\n");
@@ -27,11 +47,25 @@ int main() {
 	RCC_AHB1ENR |= RCC_GPIODEN;
 	RCC_APB1ENR |= RCC_TIM4EN;
 
-	// initialization
+		// initialization
+	// GPIO init
+	set_gpiodMode(GREEN_LED, 0b01);
+	set_gpiodType_pushpull(GREEN_LED);
+	turnd_off(GREEN_LED);
+
+	//init pin button A0
+	GPIOA_MODER = SET_BITS(GPIOA_MODER, 0*2, 2, 0b00);
+	GPIOA_OTYPER &= ~(1<<0);
+	GPIOA_PUPDR =  SET_BITS(GPIOA_PUPDR, 0*2, 2, 0b00);
 
 	// main loop
 	printf("Endless loop!\n");
 	while(1) {
+		if((GPIOA_IDR & (1<<USER_BUT)) != 0){
+			turnd_on(GREEN_LED);
+		}else{
+			turnd_off(GREEN_LED);
+		}
 	}
 
 }
